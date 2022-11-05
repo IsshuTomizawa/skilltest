2edda049014b31e8ab979ad64a13292415b82835
Ryohei Takatsuji <s1F102001669@iniad.org>
2022-11-05 17:48:13 +0900

    サイト完成
    あとは実装

--- skil/models.py
+++ skil/models.py
@@ -2,37 +2,40 @@ from email.policy import default
 from django.db import models
 
 # Create your models here.
-'''
-class Wquestion(models.Model):
-    ques = models.TextField()
-    answer = models.IntegerField()
-
-    def __str__(self):
-        return self.ques
-'''
-
 class Equestion(models.Model):
     number = models.IntegerField(default=0)
     ques = models.TextField()
     sele1 = models.TextField()
     sele2 = models.TextField()
     sele3 = models.TextField()
-    answer = models.IntegerField()
+    answer = models.TextField()
 
     def __str__(self):
         return self.ques
 
 class Nquestion(models.Model):
+    number = models.IntegerField(default=0)
     ques = models.TextField()
-    answer = models.IntegerField()
+    answer = models.CharField(max_length=1)
+    com = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return self.ques
 
-class Percent(models.Model):
+class Epercent(models.Model):
     ques = models.TextField()
     true = models.IntegerField(default = 0)
     false = models.IntegerField(default = 0)
+    percent = models.IntegerField(default = 0)
+
+    def __str__(self):
+        return self.ques
+
+class Npercent(models.Model):
+    ques = models.TextField()
+    true = models.IntegerField(default = 0)
+    false = models.IntegerField(default = 0)
+    percent = models.IntegerField(default = 0)
 
     def __str__(self):
         return self.ques
